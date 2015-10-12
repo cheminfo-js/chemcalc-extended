4030b6d5c1bedcb92e942fab20339c39c44e33a3
Luc Patiny <luc@patiny.com>
2015-10-12 16:41:37 +0200

    Update

--- src/MFProcessorWorker.js
+++ src/MFProcessorWorker.js
@@ -6,78 +6,64 @@ var bestResults = require('./bestResults');
 
 module.exports = MFProcessorWorker;
 
-function MFProcessorWorker(options) {
-    if (!options.url) {
-        throw new Error('URL to the chemcalc extended library is needed');
+function MFProcessorWorker(managerOptions) {
+    if (!managerOptions.url) {
+        throw new Error('URL to the chemcalc extended library is required');
     }
-    options.deps = [options.url];
-    this.options = options;
+    managerOptions.deps = [managerOptions.url];
+    this.managerOptions = managerOptions;
     this.manager = null;
-    this.hasExperimental = false;
     this._init();
 }
 
 MFProcessorWorker.prototype._init = function () {
-    this.hasExperimental = false;
-    this.manager = new WorkerManager(insideMFProcessor, this.options);
+    this.manager = new WorkerManager(insideMFProcessor, this.managerOptions);
 };
 
-MFProcessorWorker.prototype.callbacks = function (options) {
-    options = options || {};
-    this.onStep = options.onStep || Function.prototype;
-    this.onError = options.onError || Function.prototype;
+MFProcessorWorker.prototype.callbacks = function (callBacksOptions) {
+    callBacksOptions = callBacksOptions || {};
+    this.onStep = callBacksOptions.onStep || Function.prototype;
+    this.onError = callBacksOptions.onError || Function.prototype;
 };
 
 MFProcessorWorker.prototype.init = function (experimental, calculationOptions) {
-    this.hasExperimental = true;
+    this.calculationOptions=calculationOptions;
     return this.manager.postAll('init', [experimental, calculationOptions]);
 };
 
-MFProcessorWorker.prototype.process = function (mfs, experimental, calculationOptions) {
+MFProcessorWorker.prototype.process = function (mfs) {
     if (!Array.isArray(mfs)) {
         throw new TypeError('mfs must be an array');
     }
-    if (!Array.isArray(experimental)) {
-        calculationOptions = experimental;
-        experimental = null;
-    }
-    var promise;
-    if (experimental) {
-        promise = this.init(experimental, calculationOptions);
-    } else {
-        promise = Promise.resolve();
-    }
     var self = this;
-    return promise.then(function () {
-        return new Promise(function (resolve, reject) {
-            var processed = 0;
-            var total = mfs.length;
-            var results = [];
-            mfs.forEach(function (mf) {
-                self.manager.post('data', [mf.mf]).then(
-                    function (result) {
-                        result.parts = mf;
-                        results.push(result);
-                        if (results.length > calculationOptions.maxResults * 2) {
-                            results = bestResults(results, calculationOptions.bestOf, calculationOptions.maxResults, calculationOptions.minSimilarity);
-                        }
-
-                    },
-                    self.onError
-                ).then(function () {
-                    processed++;
-                    self.onStep(processed, total);
-                    if (processed === total) {
-                        results = bestResults(results, calculationOptions.bestOf, calculationOptions.maxResults, calculationOptions.minSimilarity);
-                        results=results.map(function(a) {
-                            var ppm=((a.msem-calculationOptions.mass)/(a.msem))*1e6;
-                            a.ppm=Math.round(ppm*100)/100;
-                            a.absppm=Math.abs(a.ppm*100)/100;
-                            return a;
-                        });
-                        resolve(results);
+    return new Promise(function (resolve, reject) {
+        var processed = 0;
+        var total = mfs.length;
+        var results = [];
+        mfs.forEach(function (mf) {
+            self.manager.post('data', [mf.mf]).then(
+                function (result) {
+                    result.parts = mf;
+                    results.push(result);
+                    if (results.length > self.calculationOptions.maxResults * 2) {
+                        results = bestResults(results, self.calculationOptions.bestOf, self.calculationOptions.maxResults, self.calculationOptions.minSimilarity);
                     }
-                });
+
+                },
+                self.onError
+            ).then(function () {
+                processed++;
+                self.onStep(processed, total);
+                if (processed === total) {
+                    results = bestResults(results, self.calculationOptions.bestOf, self.calculationOptions.maxResults, self.calculationOptions.minSimilarity);
+                    results=results.map(function(a) {
+                        var ppm=((a.msem-self.calculationOptions.mass)/(a.msem))*1e6;
+                        a.ppm=Math.round(ppm*100)/100;
+                        a.absppm=Math.abs(a.ppm*100)/100;
+                        return a;
+                    });
+                    resolve(results);
+                }
             });
         });
     });
