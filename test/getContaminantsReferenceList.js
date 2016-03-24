4232ac47f1b6c67f5d998b98860dbdb6d4e8da1b
lpatiny <lpatiny@users.noreply.github.com>
2016-03-24 08:31:50 +0100

    Update getContaminantsReferenceList.js

--- test/getContaminantsReferenceList.js
+++ test/getContaminantsReferenceList.js
@@ -5,7 +5,7 @@ var CE = require('..');
 
 describe('Test getContaminantReference', function () {
     it('Check the json', function () {
-        this.timeout(5000);
+        this.timeout(20000);
         return CE.getContaminantsReferenceList().then(function(result) {
             result.length.should.be.above(1000);
             result[0].em.should.be.above(190);
