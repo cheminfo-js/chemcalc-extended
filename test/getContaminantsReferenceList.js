0cbba343c2d3f324f8cb0577aca6fbdb87f2aef1
Luc Patiny <luc@patiny.com>
2016-03-24 07:54:42 +0100

    Add more time for test

--- test/getContaminantsReferenceList.js
+++ test/getContaminantsReferenceList.js
@@ -5,7 +5,7 @@ var CE = require('..');
 
 describe('Test getContaminantReference', function () {
     it('Check the json', function () {
-
+        this.timeout(5000);
         return CE.getContaminantReference().then(function(result) {
             result.length.should.be.above(1000);
             result[0].em.should.be.above(190);
