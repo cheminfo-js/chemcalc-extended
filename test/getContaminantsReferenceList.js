212fc4bbc70837efe172ed5015907b18792915f8
Luc Patiny <luc@patiny.com>
2016-04-25 13:42:04 +0200

    Add retries in Mocha

--- test/getContaminantsReferenceList.js
+++ test/getContaminantsReferenceList.js
@@ -6,6 +6,7 @@ var CE = require('..');
 describe('Test getContaminantsReferenceList', function () {
     it('Check the json', function () {
         this.timeout(20000);
+        this.retries(5);
         return CE.getContaminantsReferenceList().then(function(result) {
             result.length.should.be.above(1000);
             result[0].em.should.be.above(190);
