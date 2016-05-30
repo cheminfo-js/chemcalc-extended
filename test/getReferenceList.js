54fed05df7e0f99351ab5bfd4593515e1440026a
Luc Patiny <luc@patiny.com>
2016-05-30 14:56:06 +0200

    Add testcase for file not found

--- test/getReferenceList.js
+++ test/getReferenceList.js
@@ -4,7 +4,7 @@ var CE = require('..');
 
 
 describe('Test getReferenceList', function () {
-    it('Check the json', function () {
+    it('Check the existing json', function () {
         this.timeout(20000);
         this.retries(5);
         return CE.getReferenceList('https://googledocs.cheminfo.org/spreadsheets/d/15Kuc5MeOhvm4oeTMvEuP1rWdRFiVWosxXhYwAmuf3Uo/export?format=tsv').then(function(result) {
@@ -12,5 +12,16 @@ describe('Test getReferenceList', function () {
             result[0].em.should.be.equal(22.0782503207);
         });
     });
+
+    it('Check the non existing json', function () {
+        this.timeout(20000);
+        this.retries(5);
+        return CE.getReferenceList('https://googledocs.cheminfo.org/spreadsheets/d/15Kuc5MeOhvm4xxxVWosxXhYwAmuf3Uo/export?format=tsv').then(function(result) {
+            console.log(result);
+        }).catch(function(error) {
+            error.response.status.should.equal(404);
+        });
+    });
+    
 });
 
