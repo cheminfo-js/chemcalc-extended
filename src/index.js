d58af76bfa3eec5bc4276c4f1eb26ec64d80a156
Luc Patiny <luc@patiny.com>
2017-01-09 13:31:42 +0100

    Fix worker

--- src/index.js
+++ src/index.js
@@ -27,7 +27,36 @@ CE.getInfo = CC.getInfo;
 CE.mfFromMonoisotopicMass = CC.mfFromMonoisotopicMass;
 
 
-CE.mfFromMonoisotopicMassSimilarity = require('./mfFromMonoisotopicMassSimilarity');
+
+/*
+ mfFromMonoisotopicMassSimilarity
+ We will extend mfFromMonoisotopicMass in order to include in the options:
+ * experimental : an array of [[x1,y1],[x2,y2],...] or [[x1,x2,x3,...][y1,y2,y3,...]]
+ * widthTop : top width of the trapezoid
+ * widthBottom : bottom width of the trapezoid
+ * from : mass "from" which calculate the similarity
+ * to : mass "to" which calculate the similarity
+ * As an alternative the from / to parameters can be calculated based target mass
+ *
+ * decimalsPPM : number of decimals for PPM
+ * decimalsMass : number odecimals for the mass
+ */
+// TODO : May not be moved (and create it's own file) because it also has to work inside a worker !!!!!
+CE.mfFromMonoisotopicMassSimilarity = function mfFromMonoisotopicMassSimilarity (mass, experimental, options) {
+    var mfResults = CC.mfFromMonoisotopicMass(mass, options);
+    var processor = new MFProcessor(experimental, options);
+
+    var results = mfResults.results;
+    for (var i = 0; i < results.length; i++) {
+        var result = results[i];
+        processor.process(result.mf.value || result.mf, result);
+    }
+    mfResults.extractExperimental = processor.similarity.getExtract1();
+
+    mfResults.results = bestResults(results, options.bestOf, options.maxResults, options.minSimilarity);
+    return mfResults;
+};
+
 
 CE.pubchemStats={};
 CE.getPubchemStats=function (id) {
